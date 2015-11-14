d1622fef106d5adbc5a12456f24eaaafb13b1b27
Andy White <andywhite87@gmail.com>
2015-11-14 22:42:00 +0000

    Add image variables, make minifig folder configurable via data-attr

--- src/js/static.js
+++ src/js/static.js
@@ -3,6 +3,10 @@
 
   LegoIpsum.IMAGE = 1;
 
+  LegoIpsum.TOTAL_IMAGES = 55;
+
+  LegoIpsum.IMAGE_FOLDER = './img/figs/';
+
   LegoIpsum.TEXT = 2;
 
   LegoIpsum.TYPE = {
@@ -10,3 +14,15 @@
     SENTENCE: 2,
     WORD: 3
   };
+
+  // Overwrite variables based on data-attrs
+
+  (function () {
+    var scriptTag = document.querySelector('script[data-minifig-path]');
+    if (scriptTag !== null) {
+      var minifigPath = scriptTag.getAttribute("data-minifig-path");
+      if (typeof minifigPath !== 'undefined' && minifigPath !== null) {
+        LegoIpsum.IMAGE_FOLDER = minifigPath;
+      }
+    }
+  })();
