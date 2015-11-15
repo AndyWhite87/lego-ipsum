8de80765951b0a56574f55a5e2a76b7f95be4bf4
Andy White <andywhite87@gmail.com>
2015-11-15 20:49:42 +0000

    Add declarative API for image method

--- src/js/declarativeApi.js
+++ src/js/declarativeApi.js
@@ -3,62 +3,82 @@
 
   (function () {
 
-    var legoIpsumEls = document.querySelectorAll('[lego-ipsum]');
+    // Declarative attributes
+    var attrs = {
+      core: 'lego-ipsum'
+    };
+    attrs.count = attrs.core + '-count';
+    attrs.format = attrs.core + '-format';
+
+    // Run declarative functionality if one or more Lego Ipsum elements are present
+    var legoIpsumEls = document.querySelectorAll('[' + attrs.core + ']');
     if (legoIpsumEls === null) {
       return;
     }
 
-    var getType = function(el) {
-      var providedType = el.getAttribute('lego-ipsum').toLowerCase();
-      var defaultType = 'phrase';
+    var getRequest = function(el) {
+      var providedRequest = el.getAttribute(attrs.core).toLowerCase();
+      var defaultRequest = 'phrase';
 
-      switch (providedType) {
+      switch (providedRequest) {
+        case 'image':
         case 'word':
         case 'phrase':
         case 'sentence':
         case 'paragraph':
-          return providedType;
+          return providedRequest;
         default:
-          return defaultType;
+          return defaultRequest;
       }
     };
 
-    var getQuery = function(el, type) {
-      var count = parseInt(el.getAttribute('lego-ipsum-count')) || 1;
+    var getType = function(request) {      
+      switch (request) {
+        case 'image':
+          return 'IMAGE';
+        default:
+          return 'TEXT';
+      }
+    };
 
-      switch (type) {
-        case 'word':
-        case 'phrase':
-          return count + 'w';
+    var getQuery = function(el, request) {
+      var count = parseInt(el.getAttribute(attrs.count)) || 1;
+
+      switch (request) {
+        case 'image':
+          return count;
         case 'sentence':
           return count + 's';
         case 'paragraph':
           return count + 'p';
+        default:
+          return count + 'w';        
       }
     };
 
-    var getSentenceCase = function(el, type) {
-      if (type === 'sentence' || type === 'paragraph') {
-        return false;
+    var getSentenceCase = function(el, request) {
+      if (request === 'word' || request === 'phrase') {
+        return el.getAttribute(attrs.format) !== null;
       }
-      return el.getAttribute('lego-ipsum-sentence-format').toLowerCase() === 'true';
+      return false;
     };
 
     for (var i = 0; i < legoIpsumEls.length; i++) {
 
       // Get each element's type and infer a query from it and whether to use sentence casing 
       var el = legoIpsumEls[i];
-      var type = getType(el);
-      var query = getQuery(el, type);
-      var sentenceCase = getSentenceCase(el, type);
+      var request = getRequest(el);
+      var query = getQuery(el, request);
+      var sentenceCase = getSentenceCase(el, request);
+      var type = getType(request);
 
       // Run LegoIpsum on the element with requested settings
       var legoIpsum = new LegoIpsum();
-      legoIpsum.type = LegoIpsum.TEXT;
+      legoIpsum.type = LegoIpsum[type];
       legoIpsum.query = query;
       legoIpsum.createLegoIpsum(el);
 
-      // Transform element's text to sentence format if requested
+      // Transform element's text to sentence format if requested and applicable
       if (sentenceCase) {
         el.textContent = el.textContent.substr(0, 1).toUpperCase() + el.textContent.substr(1);
         el.textContent = el.textContent + '.';
