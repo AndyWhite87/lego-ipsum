33194c8b76436da7914b0c5ab6c942138eeafc77
Andy White <andy.white@experian.com>
2015-11-16 13:24:19 +0000

    Refresh built files

--- src/js/concat/lego-ipsum.js
+++ src/js/concat/lego-ipsum.js
@@ -235,30 +235,6 @@ var LegoIpsum;
 
   };
 
-  // Register jQuery plugin
-  if (typeof jQuery !== 'undefined') {
-
-    (function($) {
-      
-      $.fn.legoIpsum = function() {
-
-        $(this).each(function() {
-          var legoIpsum = new LegoIpsum();
-          legoIpsum.type = $(this).is('img') ? LegoIpsum.IMAGE : LegoIpsum.TEXT;
-          legoIpsum.query = $(this).data('lego');
-          legoIpsum.createLegoIpsum(this);
-        });
-
-      };
-
-      $(document).ready(function() {
-        $('[data-lego]').lorem();
-      });
-
-    })(jQuery);
-    
-  }
-
   // Find and act upon LegoIpsum declarations
 
   (function () {
